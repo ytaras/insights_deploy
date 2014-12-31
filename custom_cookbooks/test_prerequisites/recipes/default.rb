77aed15d1b475eef50428d08e37e931f3a2ba54b
Pavlo Tilinin <ptilin@softserveinc.com>
2014-12-31 15:11:04 +0200

    fixed config for x server and nginx

--- custom_cookbooks/test_prerequisites/recipes/default.rb
+++ custom_cookbooks/test_prerequisites/recipes/default.rb
@@ -1,1 +1,3 @@
 execute 'npm install -g cucumber'
+execute 'npm install -g bower'
+execute 'npm install -g grunt-cli'
--- custom_cookbooks/test_prerequisites/recipes/pre_cook.rb
+++ custom_cookbooks/test_prerequisites/recipes/pre_cook.rb
@@ -21,3 +21,63 @@ apt_repository "pgdg" do
 	trusted false
 	deb_src false
 end
+
+
+# This installs and automatically starts bare Xorg server for ability to run google-chrome
+if ! ::File.exists? ("/etc/init/xorgserver.conf")
+	execute "apt_get_update" do
+		user "root"
+		group "root"
+		command "/usr/bin/env apt-get update"
+	end
+	package "xserver-xorg" do
+		action :install
+	end
+	cookbook_file "xorgserver.conf" do 
+		path "/etc/init/xorgserver.conf"
+		owner "root"
+		group "root"
+		mode "0644"
+		action :create
+	end
+	execute "start_x_server" do
+		user "root"
+		group "root"
+		command "/usr/bin/env service xorgserver start"
+	end
+end
+
+if ! ::File.exists? ("/opt/nginx/conf/sites-available/insights.conf")
+	cookbook_file "nginx.conf" do
+		path "/opt/nginx/conf/nginx.conf"
+		owner "nginx"
+		group "nginx"
+		action :create
+		mode "0644"
+	end
+	["sites-enabled","sites-available"].each do |d|
+		directory "/opt/nginx/conf/#{ d }" do
+			action :create
+			owner "nginx"
+			group "nginx"
+			mode "0755"
+		end
+	end
+	cookbook_file "insights.conf" do
+		path "/opt/nginx/conf/sites-available/insights.conf"
+		owner "nginx"
+		group "nginx"
+		action :create
+		mode "0644"
+	end
+	link "/opt/nginx/conf/sites-enabled/insights.conf" do
+		to "/opt/nginx/conf/sites-available/insights.conf"
+		link_type :symbolic
+	end
+	execute "restart_nginx" do
+		user "root"
+		group "root"
+		command "/usr/bin/env service nginx restart"
+	end
+end
+
